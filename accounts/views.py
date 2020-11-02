b785df66f563fca456784421480e1944a37cfe8b
Dacker Daw <dacker.daw@gmail.com>
2020-11-02 13:13:52 +0700

    Finished part 9

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_orders = orders.count()
+    total_customers = customers.count()
+
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+
+    context = {
+        'orders':orders,
+        'customers':customers,
+        'total_orders':total_orders,
+        'total_customers':total_customers,
+        'delivered':delivered,
+        'pending':pending
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+    return render(request, 'accounts/products.html', {'products':products})
+
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {
+        'customer':customer,
+        'orders':orders,
+        'order_count':order_count
+    }
+    return render(request, 'accounts/customer.html', context)
