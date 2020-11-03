759e639f210afe8e7803c73a395b8e8ccf80e0e3
Dacker Daw <dacker.daw@gmail.com>
2020-11-03 10:57:16 +0700

    Finished part 12

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.forms import inlineformset_factory
 
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 # Create your views here.
 
@@ -38,10 +39,14 @@ def customer(request, pk):
     orders = customer.order_set.all()
     order_count = orders.count()
 
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
     context = {
         'customer':customer,
         'orders':orders,
-        'order_count':order_count
+        'order_count':order_count,
+        'myFilter':myFilter
     }
     return render(request, 'accounts/customer.html', context)
 
