ace528af82d009ff97be832b80b73ef33655912f
dackerdaw <38307732+dackerdaw@users.noreply.github.com>
2020-11-07 08:33:47 +0700

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(h*knptw+hu3k%=z6f%4l3(*z8w@3mf3#x#jy2v5x$sv&vtc9*'
+SECRET_KEY = '*********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ibrahim.qardhawi@student.president.ac.id'
-EMAIL_HOST_PASSWORD = '20010720'
+EMAIL_HOST_USER = '************'
+EMAIL_HOST_PASSWORD = '***********'
